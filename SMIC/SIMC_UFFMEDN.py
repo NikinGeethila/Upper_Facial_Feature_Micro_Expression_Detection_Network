b313a9865e8f4c2be190f3f72c184a22fa1485b7
NikinGeethila <nikingeethila@gmail.com>
2020-06-19 13:20:32 +0800

    Update SIMC_UFFMEDN.py

--- SMIC/SIMC_UFFMEDN.py
+++ SMIC/SIMC_UFFMEDN.py
@@ -35,8 +35,8 @@ def evaluate(segment_train_images, segment_validation_images, segment_train_labe
 
     filepath="weights_late_microexpfusenet/weights-improvement"+str(test_index)+"-{epoch:02d}-{val_acc:.2f}.hdf5"
     checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-    EarlyStop=EarlyStopping(monitor='val_acc',min_delta=0,patience=80,restore_best_weights=True,verbose=1)
-    reduce =ReduceLROnPlateau(monitor='val_acc',factor=0.5,patience=60,verbose=1,min_delta=0)
+    EarlyStop=EarlyStopping(monitor='val_acc',min_delta=0,patience=80,restore_best_weights=True,verbose=1, mode='max')
+    reduce =ReduceLROnPlateau(monitor='val_acc',factor=0.5,patience=60,verbose=1,min_delta=0, mode='max')
     callbacks_list = [checkpoint,EarlyStop,reduce]
 
 
