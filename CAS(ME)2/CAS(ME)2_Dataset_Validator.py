075b13f1c3ef263898acd4e820b4dfcf06722a12
NikinGeethila <nikingeethila@gmail.com>
2020-06-20 16:40:40 +0800

    Update CAS(ME)2_Dataset_Validator.py

--- CAS(ME)2/CAS(ME)2_Dataset_Validator.py
+++ CAS(ME)2/CAS(ME)2_Dataset_Validator.py
@@ -4,12 +4,12 @@ import os
 segmentName='UpperFace'
 sizeH=32
 sizeV=32
-sizeD=30
+sizeD=9
 
 
 segment_traininglabels = numpy.load('numpy_training_datasets/{0}_labels_{1}x{2}x{3}.npy'.format(segmentName,sizeH, sizeV,sizeD))
 print(segment_traininglabels)
-cat=[0]*7
+cat=[0]*3
 for item in segment_traininglabels:
     for c in range(len(cat)):
         if item[c]==1:
@@ -18,16 +18,12 @@ for item in segment_traininglabels:
 print(cat)
 
 
-angerpath = '../../../Datasets/SAMM_categorical/Anger/'
-sadnesspath = '../../../Datasets/SAMM_categorical/Sadness/'
-happinesspath = '../../../Datasets/SAMM_categorical/Happiness/'
-disgustpath = '../../../Datasets/SAMM_categorical/Disgust/'
-fearpath = '../../../Datasets/SAMM_categorical/Fear/'
-surprisepath = '../../../Datasets/SAMM_categorical/Surprise/'
-contemptpath = '../../../Datasets/SAMM_categorical/Contempt/'
-otherpath = '../../../Datasets/SAMM_categorical/Other/'
-paths=[angerpath, sadnesspath, happinesspath,disgustpath,fearpath,surprisepath,contemptpath,otherpath]
-cat=[0]*8
+negativepath = '../../../Datasets/CAS(ME)2_categorical/Negative/'
+positivepath = '../../../Datasets/CAS(ME)2_categorical/Positive/'
+surprisepath = '../../../Datasets/CAS(ME)2_categorical/Surprise/'
+paths=[negativepath, positivepath, surprisepath]
+
+cat=[0]*3
 dir=0
 for typepath in (paths):
     directorylisting = os.listdir(typepath)
