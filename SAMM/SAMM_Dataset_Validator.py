6ecd0c45d9995bbaebca6c1d842bc9c9289142b9
NikinGeethila <nikingeethila@gmail.com>
2020-06-22 08:54:39 +0800

    Update SAMM_Dataset_Validator.py

--- SAMM/SAMM_Dataset_Validator.py
+++ SAMM/SAMM_Dataset_Validator.py
@@ -6,18 +6,6 @@ sizeH=32
 sizeV=32
 sizeD=30
 
-
-segment_traininglabels = numpy.load('numpy_training_datasets/{0}_labels_{1}x{2}x{3}.npy'.format(segmentName,sizeH, sizeV,sizeD))
-print(segment_traininglabels)
-cat=[0]*7
-for item in segment_traininglabels:
-    for c in range(len(cat)):
-        if item[c]==1:
-            cat[c]+=1
-
-print(cat)
-
-
 angerpath = '../../../Datasets/SAMM_categorical/Anger/'
 sadnesspath = '../../../Datasets/SAMM_categorical/Sadness/'
 happinesspath = '../../../Datasets/SAMM_categorical/Happiness/'
@@ -28,7 +16,20 @@ contemptpath = '../../../Datasets/SAMM_categorical/Contempt/'
 otherpath = '../../../Datasets/SAMM_categorical/Other/'
 
 paths=[angerpath,  happinesspath,surprisepath,contemptpath,otherpath]
-cat=[0]*8
+
+segment_traininglabels = numpy.load('numpy_training_datasets/{0}_labels_{1}x{2}x{3}.npy'.format(segmentName,sizeH, sizeV,sizeD))
+print(segment_traininglabels)
+cat=[0]*len(paths)
+for item in segment_traininglabels:
+    for c in range(len(cat)):
+        if item[c]==1:
+            cat[c]+=1
+
+print(cat)
+
+
+
+cat=[0]*len(paths)
 dir=0
 for typepath in (paths):
     directorylisting = os.listdir(typepath)
