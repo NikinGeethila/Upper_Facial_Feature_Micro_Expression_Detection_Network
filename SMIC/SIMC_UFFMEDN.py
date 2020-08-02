689bdd6c3ca984c93d213a2f06494707db5c4a54
Nikin Geethila Matharaarachchi <54057518+NikinGeethila@users.noreply.github.com>
2020-08-03 03:03:28 +0800

    Update SIMC_UFFMEDN.py

--- SMIC/SIMC_UFFMEDN.py
+++ SMIC/SIMC_UFFMEDN.py
@@ -92,28 +92,53 @@ sizeD=30
 segment_training_set = numpy.load('numpy_training_datasets/{0}_images_{1}x{2}x{3}.npy'.format(segmentName,sizeH, sizeV,sizeD))
 segment_traininglabels = numpy.load('numpy_training_datasets/{0}_labels_{1}x{2}x{3}.npy'.format(segmentName,sizeH, sizeV,sizeD))
 
-'''
+
 #-----------------------------------------------------------------------------------------------------------------
 #LOOCV
 loo = LeaveOneOut()
 loo.get_n_splits(segment_training_set)
 tot=0
 count=0
+accs=[]
+accs2=[]
+
+val_labels=[]
+pred_labels=[]
 for train_index, test_index in loo.split(segment_training_set):
 
     # print(segment_traininglabels[train_index])
     # print(segment_traininglabels[test_index])
     print(test_index)
 
-    val_acc = evaluate(segment_training_set[train_index], segment_training_set[test_index],segment_traininglabels[train_index], segment_traininglabels[test_index] ,test_index)
-    tot+=val_acc
-    count+=1
+    val_acc, val_label, pred_label = evaluate(segment_training_set[train_index], segment_training_set[test_index],
+                                              segment_traininglabels[train_index], segment_traininglabels[test_index],
+                                              test_index)
+    tot += val_acc
+    val_labels.extend(val_label)
+    pred_labels.extend(pred_label)
+    accs.append(val_acc)
+    accs2.append(segment_traininglabels[test_index])
+    count += 1
     print("------------------------------------------------------------------------")
-    print("validation acc:",val_acc)
+    print("validation acc:", val_acc)
     print("------------------------------------------------------------------------")
-print(tot/count)
-print(segmentName)
-
+print("accuracy: ", accuracy_score(val_labels, pred_labels))
+cfm = confusion_matrix(val_labels, pred_labels)
+# tp_and_fn = sum(cfm.sum(1))
+# tp_and_fp = sum(cfm.sum(0))
+# tp = sum(cfm.diagonal())
+print("cfm: \n", cfm)
+# print("tp_and_fn: ",tp_and_fn)
+# print("tp_and_fp: ",tp_and_fp)
+# print("tp: ",tp)
+#
+# precision = tp / tp_and_fp
+# recall = tp / tp_and_fn
+# print("precision: ",precision)
+# print("recall: ",recall)
+# print("F1-score: ",f1_score(val_labels,pred_labels,average="macro"))
+print("F1-score: ", f1_score(val_labels, pred_labels, average="weighted"))
+'''
 
 #-----------------------------------------------------------------------------------------------------------------
 #Test train split
@@ -134,7 +159,7 @@ numpy.save('numpy_validation_datasets/{0}_images_{1}x{2}.npy'.format(segmentName
 # labels = numpy.load('numpy_validation_datasets/{0}_images_{1}x{2}.npy'.format(segmentName,sizeH, sizeV))
 
 evaluate(segment_train_images, segment_validation_images,segment_train_labels, segment_validation_labels ,0)
-'''
+
 
 #-----------------------------------------------------------------------------------------------------------------------------
 #k-fold(10)
@@ -181,7 +206,7 @@ print("cfm: \n",cfm)
 # print("recall: ",recall)
 # print("F1-score: ",f1_score(val_labels,pred_labels,average="macro"))
 print("F1-score: ",f1_score(val_labels,pred_labels,average="weighted"))
-
+'''
 
 #---------------------------------------------------------------------------------------------------
 # write to results
@@ -189,7 +214,7 @@ print("F1-score: ",f1_score(val_labels,pred_labels,average="weighted"))
 results=open("../TempResults.txt",'a')
 results.write("---------------------------\n")
 full_path = os.path.realpath(__file__)
-results.write(str(os.path.dirname(full_path))+" 10-FOLD\n")
+results.write(str(os.path.dirname(full_path))+" LOOCV\n")
 results.write("---------------------------\n")
 results.write("accuracy: "+str(accuracy_score(val_labels, pred_labels))+"\n")
 results.write("F1-score: "+str(f1_score(val_labels,pred_labels,average="weighted"))+"\n")
