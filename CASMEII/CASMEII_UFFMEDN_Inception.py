accd885614c32d4d7b7236b8ffe618dc4f9209f6
Nikin Geethila Matharaarachchi <54057518+NikinGeethila@users.noreply.github.com>
2020-08-10 05:31:54 +0800

    Update CASMEII_UFFMEDN_Inception.py

--- CASMEII/CASMEII_UFFMEDN_Inception.py
+++ CASMEII/CASMEII_UFFMEDN_Inception.py
@@ -4,7 +4,7 @@ from sklearn.metrics import accuracy_score,f1_score
 from keras.models import Sequential, Model
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Convolution3D, MaxPooling3D, ZeroPadding3D
-from keras.layers import LeakyReLU ,PReLU,Input,Add,BatchNormalization
+from keras.layers import LeakyReLU ,PReLU,Input,Add,BatchNormalization,concatenate
 from keras.callbacks import ModelCheckpoint,EarlyStopping,ReduceLROnPlateau,Callback
 from sklearn.model_selection import train_test_split,LeaveOneOut,KFold
 from keras import backend as K
@@ -20,25 +20,33 @@ class myCallback(Callback):
 def evaluate(segment_train_images, segment_validation_images, segment_train_labels, segment_validation_labels,test_index ):
 
 
-    input = Input(shape=(1, sizeH, sizeV, sizeD))
-    conv1 = Convolution3D(256, (20, 20, 9), strides=(10, 10, 3), padding='Same')(input)
-    # bn1=BatchNormalization()(conv1)
-    ract_1 = PReLU()(conv1)
-    conv2 = Convolution3D(128, (3, 3, 3), strides=1, padding='Same')(ract_1)
-    # bn2=BatchNormalization()(conv2)
-    ract_2 = PReLU()(conv2)
-    conv3 = Convolution3D(256, (3, 3, 3), strides=1, padding='Same')(ract_2)
-    # bn3=BatchNormalization()(conv3)
-    add1= Add() ([conv3,ract_1])
-    conv3 = Convolution3D(512, (3, 3, 3), strides=1, padding='Same')(add1)
+    layer_in = Input(shape=(1, sizeH, sizeV, sizeD))
+    # conv1 = Convolution3D(256, (20, 20, 9), strides=(10, 10, 3), padding='Same')(input)
+    # # bn1=BatchNormalization()(conv1)
+    # ract_1 = PReLU()(conv1)
+    conv1 = Convolution3D(64,  (20, 20, 30), strides=(10, 10, 15), padding='same', activation='relu')(layer_in)
+    # 3x3 conv
+    conv3 = Convolution3D(96,  (20, 20, 30), strides=(10, 10, 15), padding='same', activation='relu')(layer_in)
+    conv3 = Convolution3D(128, (3, 3,3), padding='same', activation='relu')(conv3)
+    # 5x5 conv
+    conv5 = Convolution3D(16,  (20, 20, 30), strides=(10, 10, 15), padding='same', activation='relu')(layer_in)
+    conv5 = Convolution3D(32, (5, 5,5), padding='same', activation='relu')(conv5)
+    # 3x3 max pooling
+    pool = MaxPooling3D((3, 3,3), strides=(1, 1,1), padding='same')(layer_in)
+    pool = Convolution3D(32,  (20, 20, 30), strides=(10, 10, 15), padding='same', activation='relu')(pool)
+    # concatenate filters, assumes filters/channels last
+    layer_out = concatenate([conv1, conv3, conv5, pool], axis=-4)
+    # add1= Add() ([conv3,ract_1])
+    drop0 = Dropout(0.5)(layer_out)
+    conv6 = Convolution3D(512, (3, 3, 3), strides=1, padding='Same')(drop0)
     # bn3 = BatchNormalization()(conv3)
-    ract_3 = PReLU()(conv3)
-    flatten_1 = Flatten()(ract_3)
+    ract_4 = PReLU()(conv6)
+    flatten_1 = Flatten()(ract_4)
     dense_1 = Dense(5, init='normal')(flatten_1)
     drop1 = Dropout(0.5)(dense_1)
     activation = Activation('softmax')(drop1)
     opt = SGD(lr=0.01)
-    model = Model(inputs=[input], outputs=activation)
+    model = Model(inputs=[layer_in], outputs=activation)
     model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
 
     model.summary()
@@ -205,7 +213,7 @@ K.set_image_dim_ordering('th')
 segmentName='UpperFace'
 sizeH=64
 sizeV=64
-sizeD=30
+sizeD=141
 testtype="kfold"
 ####################################
 
